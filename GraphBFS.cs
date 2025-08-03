936b6c4d39dbba60110c48005a8b7355aa83b432
Karambir <karamvir100@gmail.com>
2025-08-03 14:58:46 +0530

    Add files via upload

---
+++ GraphBFS.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BFS
+{
+    public class GraphBFS
+    {
+        private Dictionary<int, List<int>> adjList = new();
+
+        public void AddEdge(int u, int v)
+        {
+            if (!adjList.ContainsKey(u)) adjList[u] = new List<int>();
+            if (!adjList.ContainsKey(v)) adjList[v] = new List<int>(); // if undirected
+            adjList[u].Add(v);
+            adjList[v].Add(u); // for undirected graph
+        }
+
+        public void BFS(int start)
+        {
+            var visited = new HashSet<int>();
+            var queue = new Queue<int>();
+            queue.Enqueue(start);
+            visited.Add(start);
+
+            while (queue.Count > 0)
+            {
+                int current = queue.Dequeue();
+                Console.Write(current + " ");
+
+                foreach (var neighbor in adjList[current])
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        queue.Enqueue(neighbor);
+                        visited.Add(neighbor);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GraphDFS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DFS
+{
+    private Dictionary<int, List<int>> adjList = new();
+
+    public void AddEdge(int u, int v)
+    {
+        if (!adjList.ContainsKey(u)) adjList[u] = new List<int>();
+        if (!adjList.ContainsKey(v)) adjList[v] = new List<int>(); // if undirected
+        adjList[u].Add(v);
+        adjList[v].Add(u); // for undirected graph
+    }
+
+    public void DFSIterative(int start)
+    {
+        var visited = new HashSet<int>();
+        var stack = new Stack<int>();
+        stack.Push(start);
+
+        while (stack.Count > 0)
+        {
+            var node = stack.Pop();
+            if (!visited.Contains(node))
+            {
+                Console.Write(node + " ");
+                visited.Add(node);
+
+                foreach (var neighbor in adjList[node])
+                {
+                    if (!visited.Contains(neighbor))
+                        stack.Push(neighbor);
+                }
+            }
+        }
+    }
+
+}
